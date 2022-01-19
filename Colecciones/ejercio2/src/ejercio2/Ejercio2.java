fe3a562746f3d19b5303ec3ec69bb85fa8b23b50
Dario Alejandro <91350692+darioah@users.noreply.github.com>
2022-01-19 19:43:08 -0300

    Add files via upload

---
+++ Colecciones/ejercio2/src/ejercio2/Ejercio2.java
@@ -0,0 +1,28 @@
+
+package ejercio2;
+
+import java.util.Scanner;
+
+
+ 
+public class Ejercio2 {
+
+    
+    public static void main(String[] args) {
+      
+    
+      
+      Scanner teclado = new Scanner(System.in); 
+      String frase; 
+      
+      System.out.println("ingrese frase"); 
+      
+       frase = teclado.nextLine();
+      
+       
+       System.out.println("la frese ingresada es "+ frase.toUpperCase() ); 
+              
+              
+    }
+    
+}
